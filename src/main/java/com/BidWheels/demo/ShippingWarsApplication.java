6fc5e3066aac79c6a763be1ebbaa1a472d1c890d
parthvaghela <parth.vaghela@vcontractor.co.za>
2024-05-14 19:14:39 +0530

    make it working on ec2

--- src/main/java/com/BidWheels/demo/ShippingWarsApplication.java
+++ src/main/java/com/BidWheels/demo/ShippingWarsApplication.java
@@ -8,7 +8,7 @@ public class ShippingWarsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShippingWarsApplication.class, args);
-		System.out.println("hello ");
+		System.out.println("hello");
 	}
 
 }
